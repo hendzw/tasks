40b4d7ac5e4162b720e95443a60293463a6e3bb1
hendzw <hendriwfinatti@gmail.com>
2024-05-23 08:32:07 -0300

    criação da tela de tarefas (today)

---
+++ src/screens/Task_List.js
@@ -0,0 +1,32 @@
+import React, {Component} from "reac"
+import { StyleSheet, View, Text, ImageBackground } from "reac-native"
+
+import today_Image from '../../assets/imgs/today.jpg'
+
+export default class Task_List extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <ImageBackground source={today_Image}>
+
+                </ImageBackground>
+                <View>
+                    <Text>Lista de Tarefas</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    taskList:{
+        flex: 7
+    }
+
+})
