db8660f83babdadcbad1ddc98a7049aae8d52aa3
hendzw <hendriwfinatti@gmail.com>
2024-06-21 08:20:20 -0300

    save

--- src/screens/components/Task.js
+++ src/screens/components/Task.js
@@ -1,34 +1,58 @@
-import React, {Component} from "reac"
-import { StyleSheet, View, Text, ImageBackground } from "reac-native"
+import React from "react"
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native"
 import moment from "moment"
 import 'moment/locale/pt-br'
+import {Icon} from "react-native-vector-icons/FontAwesome6"
 
 export default props => {
     return(
-        <View>
-            <View>
-                <Text style={styles.desc}>{props.description}</Text>
-                <Text style={styles.date}>{props.estimate_ay}</Text>
-                <Text style={styles.date}>{props.date_at}</Text>
+        <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={() => alert("Olá")}>
+            <View style = {styles.checkContainer}>
+                <View style = {styles.done}>
+                    <Icon name="check" size={20}></Icon>
+                </View> 
             </View>
+        </TouchableWithoutFeedback>
+        <View>
+            <Text style={styles.desc}>{props.description}</Text>
+            <Text style={styles.date}>{props.estimate + ""}</Text>
+            <Text style={styles.date}>{props.date_at + ""}</Text>
+        </View>
         </View>
     )
 }
 
-const styles = StylesSheet.create({
+const styles = StyleSheet.create({
     container:{
         borderColor: '#AAA',
         borderBottomWidth: 1,
-        alightItens: 'center',
+        alignItems: 'center',
         paddingVertical: 20
     },
     desc:{
-        fontFamily: 'Arial',
-        fontSize: 20,
-        color: '#333'
+        fontFamily:'Arial',
+        frontSize: 20,
+        color:'#333'
     },
     date:{
         fontFamily: 'Arial',
-        color: '#555'
+        color: '#555',
+
+    },
+    checkContainer:{
+        width: '20%',
+        aligntItems: 'center',
+        justifyContent: 'center',
+    },
+    done:{
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        backgroundColor: '#4D7031',
+        lignItems: 'center',
+        justifyContent: 'center'
     }
+
 })
