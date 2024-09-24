f1206d9e05b3922e0b9dcdbda6b9d00e4044d1ae
hendzw <hendriwfinatti@gmail.com>
2024-09-24 08:44:21 -0300

    addBotoes

--- src/screens/Add_Tarefa.js
+++ src/screens/Add_Tarefa.js
@@ -8,34 +8,46 @@ import {
     TouchableWithoutFeedback,
     Modal
 } from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome6"
+import moment from "moment"
+import DateTimePicker from "@react-native-community/datetimepicker"
 
 export default class Add_Tarefa extends Component {
     render() {
         return (
             <Modal
                 transparent={true}
-                visible={true}
+                visible={this.props.visivel}
+                onRequestClose={this.props.cancelar}
                 animationType="slide"
             >
-                <TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={this.props.cance.cancelar}>
                     <View style={styles.fundo}></View>
                 </TouchableWithoutFeedback>
                 <View style={styles.principal}>
                     <Text style={styles.cabecalho}>Nova Tarefa</Text>
-                    <TextInput
-                        placeholder="Descrição da tarefa"
-                    />
-                    <View>
+                    <View style={styles.container}>
+                        <Icon name="clipboard-check" size={25}></Icon>
+                        <TextInput
+                            style = {styles.input}
+                            placeholder="Descrição da tarefa"
+                        />
+                    </View>
+                    <TouchableOpacity style={styles.container}>
+                        <Icon name="calendar" size={25}></Icon>
+                        <Text style={styles.data}>Data Atual</Text>
+                    </TouchableOpacity>
+                    <View style={styles.botoes}>
                         <TouchableOpacity>
-                            <Text>Cancelar</Text>
+                            <Text style={styles.botao}>Cancelar</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text>Salvar</Text>
+                        <TouchableOpacity onPress={this.props.cance.cancelar}>
+                            <Text style={styles.botao}>Salvar</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
 
-                <TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={this.props.cance.cancelar}>
                     <View style={styles.fundo}></View>
                 </TouchableWithoutFeedback>
             </Modal>
@@ -58,5 +70,30 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 20,
         fontSize: 25
+    },
+    input:{
+        width: '85%',
+        margin: 15,
+        borderWidth: 1,
+        borderColor: 'E3E3E3',
+        borderRadius: 6
+    },
+    container:{
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginLeft: 15,
+      paddingVertical: 10
+    },
+    botoes:{
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    botao:{
+        margin: 20,
+        marginRight: 39,
+        color: 'B13B44'
+    },
+    data:{
+        margin: 10
     }
 })
